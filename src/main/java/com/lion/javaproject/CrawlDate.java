f40f988c51042e466e03d8105db1a7982bd922a8
xxo <xxoxec@gmail.com>
2023-04-20 19:03:42 +0900

    feat: 마지막 크롤 시간 작업 -CrawlDate 도메인 추가

---
+++ src/main/java/com/lion/javaproject/CrawlDate.java
@@ -0,0 +1,13 @@
+package com.lion.javaproject;
+
+import java.time.LocalDateTime;
+
+public class CrawlDate {
+    private LocalDateTime lastCrawlDateTime;
+    // alt + insert
+
+    public CrawlDate(LocalDateTime lastCrawlDateTime) {
+        this.lastCrawlDateTime = lastCrawlDateTime;
+    }
+
+}
