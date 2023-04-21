3fcbcf96504840bb77c2d7ebd81e2d9de1c4291d
xxo <xxoxec@gmail.com>
2023-04-21 15:30:40 +0900

    feat: User - User 수정 - UserTest 수정

--- src/main/java/com/lion/javaproject/week1/day5/User.java
+++ src/main/java/com/lion/javaproject/week1/day5/User.java
@@ -7,6 +7,7 @@ public class User {
     int age;
 
     boolean isAdult() {
+
         return  age >= 18;
     }
 }
--- src/main/java/com/lion/javaproject/week1/day5/UserTest.java
+++ src/main/java/com/lion/javaproject/week1/day5/UserTest.java
@@ -3,7 +3,7 @@ package com.lion.javaproject.week1.day5;
 public class UserTest {
     public static void main(String[] args) {
         User kyungeun = new User();
-        kyungeun.name = "김경은";
+        kyungeun.name = "경은";
         kyungeun.phoneNumber = "010-0000-1234";
         kyungeun.age = 36;
 
@@ -12,6 +12,8 @@ public class UserTest {
         su.phoneNumber = " 010-0000-1235";
         su.age = 18;
 
-
+        System.out.printf("%s님은 성인입니까 ? %s\n", kyungeun.name, kyungeun.isAdult());
+        System.out.printf("%s님은 성인입니까 ? %s\n", su.name, su.isAdult());
     }
+
 }
