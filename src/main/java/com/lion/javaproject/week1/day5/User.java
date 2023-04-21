e8a117f6762869cc0015407698c27218954ac180
xxo <xxoxec@gmail.com>
2023-04-21 15:21:01 +0900

    feat: User - User - UserTest

---
+++ src/main/java/com/lion/javaproject/week1/day5/User.java
@@ -0,0 +1,12 @@
+package com.lion.javaproject.week1.day5;
+
+public class User {
+    // 이름, 나이, 전화번호
+    String name;
+    String phoneNumber;
+    int age;
+
+    boolean isPrime() {
+        return  age >= 18;
+    }
+}
---
+++ src/main/java/com/lion/javaproject/week1/day5/UserTest.java
@@ -0,0 +1,17 @@
+package com.lion.javaproject.week1.day5;
+
+public class UserTest {
+    public static void main(String[] args) {
+        User kyungeun = new User();
+        kyungeun.name = "김경은";
+        kyungeun.phoneNumber = "010-0000-1234";
+        kyungeun.age = 36;
+
+        User su = new User();
+        su.name = "수";
+        su.phoneNumber = " 010-0000-1235";
+        su.age = 18;
+
+
+    }
+}
