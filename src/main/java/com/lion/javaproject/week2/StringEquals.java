73ef26ba4a3ec95edc30d15605ea2d447680f4fb
xxo <xxoxec@gmail.com>
2023-04-25 15:02:33 +0900

    feat: StringEquals - 문자열 비교

---
+++ src/main/java/com/lion/javaproject/week2/StringEquals.java
@@ -0,0 +1,10 @@
+package com.lion.javaproject.week2;
+
+public class StringEquals {
+    public static void main(String[] args) {
+        String str1 = "GOLD";
+        String str2 = "G";
+        System.out.println(str1.substring(0, 1).equals(str2));
+    }
+
+}
