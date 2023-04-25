8c083a09efa3374b6fe72e345af5aab8b4eefd47
xxo <xxoxec@gmail.com>
2023-04-25 15:03:28 +0900

    feat: Arrays - Array

---
+++ src/main/java/com/lion/javaproject/week2/day2/StudentMathScore.java
@@ -0,0 +1,16 @@
+package com.lion.javaproject.week2.day2;
+
+import java.util.Arrays;
+
+public class StudentMathScore {
+    public static void main(String[] args) {
+        int[] iArr = new int[5];
+        System.out.println(iArr[0]);
+        System.out.println(iArr[1]);
+        System.out.println(iArr[2]);
+        System.out.println(iArr[3]);
+        System.out.println(iArr[4]);
+
+        System.out.println(Arrays.toString(iArr));
+    }
+}
