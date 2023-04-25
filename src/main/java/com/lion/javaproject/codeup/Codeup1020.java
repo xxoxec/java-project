bb8b1829f16441fed1019ca80bab5a5417b968d9
xxo <xxoxec@gmail.com>
2023-04-25 11:16:33 +0900

    feat: Codeup - 1020

---
+++ src/main/java/com/lion/javaproject/codeup/Codeup1020.java
@@ -0,0 +1,13 @@
+package com.lion.javaproject.codeup;
+
+import java.util.Scanner;
+
+public class Codeup1020 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String x = sc.next();
+        String[] y = x.split("-");
+        System.out.print(y[0] + y[1]);
+
+    }
+}
