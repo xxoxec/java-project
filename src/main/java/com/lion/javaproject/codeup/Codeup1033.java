9fe8459f50f2a804fa5c8156f5d85b2dc34fbb0c
xxo <xxoxec@gmail.com>
2023-04-25 09:16:56 +0900

    feat: Codeup - 1031 1032 1033

---
+++ src/main/java/com/lion/javaproject/codeup/Codeup1033.java
@@ -0,0 +1,12 @@
+package com.lion.javaproject.codeup;
+
+import java.util.Scanner;
+
+public class Codeup1033 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        System.out.printf("%X", a);
+
+    }
+}
