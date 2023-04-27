05377096a4c447285cba6cd726a7aeaab748c7d8
xxo <xxoxec@gmail.com>
2023-04-27 10:45:52 +0900

    feat: 비교연산자

---
+++ src/main/java/com/lion/javaproject/week2/day4/ComparisonOperatorsEx.java
@@ -0,0 +1,12 @@
+package com.lion.javaproject.week2.day4;
+
+public class ComparisonOperatorsEx {
+    public static void main(String[] args) {
+
+        int iVal1 = 10;
+        int iVal2 = 20;
+        boolean result = iVal1 == iVal2;
+        System.out.printf("a와 b가 같은지? %b",result);
+    }
+
+}
