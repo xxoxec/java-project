56ce15e619469d650311a29db0552ca5a03d9f1e
xxo <xxoxec@gmail.com>
2023-04-27 15:35:43 +0900

    feat: codeup - 1063 1064

---
+++ src/main/java/com/lion/javaproject/codeup/Codeup1064.java
@@ -0,0 +1,24 @@
+package com.lion.javaproject.codeup;
+
+import java.util.Scanner;
+
+public class Codeup1064 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int val1 = sc.nextInt();
+        int val2 = sc.nextInt();
+        int val3 = sc.nextInt();
+
+        System.out.println((val1 > val2 ? val1 : val2) < val3 ? ( val1 < val2 ? val1 : val2) : val3);
+
+//        int val1 = sc.nextInt();
+//        int val2 = sc.nextInt();
+//        int val3 = sc.nextInt();
+//
+//        int min = val1;
+//        min = val2 < min ? val2 : min;
+//        min = val3 > min ? val3 : min;
+//        System.out.println(min);
+
+    }
+}
