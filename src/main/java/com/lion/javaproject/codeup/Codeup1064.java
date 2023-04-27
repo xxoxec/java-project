eb3c809ed53a5028926b3e7cec61b069dbf5854d
xxo <xxoxec@gmail.com>
2023-04-27 15:46:14 +0900

    feat: codeup - 1064

--- src/main/java/com/lion/javaproject/codeup/Codeup1064.java
+++ src/main/java/com/lion/javaproject/codeup/Codeup1064.java
@@ -9,7 +9,7 @@ public class Codeup1064 {
         int val2 = sc.nextInt();
         int val3 = sc.nextInt();
 
-        System.out.println((val1 > val2 ? val1 : val2) < val3 ? ( val1 < val2 ? val1 : val2) : val3);
+        System.out.println((val1 < val2 ? val1 : val2) < val3 ? ( val1 < val2 ? val1 : val2) : val3);
 
 //        int val1 = sc.nextInt();
 //        int val2 = sc.nextInt();
