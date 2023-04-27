1b49a97ceef3d30f89490cc0193e3d12bee25814
xxo <xxoxec@gmail.com>
2023-04-27 14:25:46 +0900

    feat: codeup - 1047 1048

---
+++ src/main/java/com/lion/javaproject/codeup/Codeup1048.java
@@ -0,0 +1,12 @@
+package com.lion.javaproject.codeup;
+
+import java.util.Scanner;
+
+public class Codeup1048 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int val1 = sc.nextInt();
+        int val2 = sc.nextInt();
+        System.out.println(val1 << val2);
+    }
+}
