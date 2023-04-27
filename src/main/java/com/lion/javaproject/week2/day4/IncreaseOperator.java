f3933b893483ef182a1df466c659c23a7ee3e8df
xxo <xxoxec@gmail.com>
2023-04-27 10:17:17 +0900

    feat: 증감연산자 - 증감연산자 올림 내림 연습

---
+++ src/main/java/com/lion/javaproject/week2/day4/IncreaseOperator.java
@@ -0,0 +1,13 @@
+package com.lion.javaproject.week2.day4;
+
+public class IncreaseOperator {
+    public static void main(String[] args) {
+        int i = 10;
+        // ++가 뒤에 오는 경우 나중에 올림,내림
+        System.out.println(i++);
+        System.out.println(i);
+
+        System.out.println(i--);
+        System.out.println(i);
+    }
+}
---
+++ src/main/java/com/lion/javaproject/week2/day4/IncreaseOperator2.java
@@ -0,0 +1,13 @@
+package com.lion.javaproject.week2.day4;
+
+public class IncreaseOperator2 {
+    public static void main(String[] args) {
+        int i = 10;
+        // ++가 앞에 오는 경우 일단 먼저 올림,내림
+        System.out.println(++i);
+        System.out.println(i);
+
+        System.out.println(--i);
+        System.out.println(i);
+    }
+}
