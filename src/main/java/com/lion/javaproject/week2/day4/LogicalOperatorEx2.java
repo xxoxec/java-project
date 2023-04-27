e4a905849358064f90e63776ba0e7e46193f1b83
xxo <xxoxec@gmail.com>
2023-04-27 11:42:38 +0900

    feat: 논리연산자2 - And(&&)

---
+++ src/main/java/com/lion/javaproject/week2/day4/LogicalOperatorEx2.java
@@ -0,0 +1,14 @@
+package com.lion.javaproject.week2.day4;
+
+public class LogicalOperatorEx2 {
+    public static void main(String[] args) {
+        boolean result = true && true;
+        System.out.printf("%b\n", result);
+        result = true && false;
+        System.out.printf("%b\n", result);
+        result = false && true;
+        System.out.printf("%b\n", result);
+        result = false && false;
+        System.out.printf("%b\n", result);
+    }
+}
