10161d7b04936c0b48a88f767b5eda6b1657dcb1
xxo <xxoxec@gmail.com>
2023-05-01 10:34:00 +0900

    feat: codeup1071 - 1071

---
+++ src/main/java/com/lion/javaproject/codeup/Codeup1071.java
@@ -0,0 +1,16 @@
+package com.lion.javaproject.codeup;
+
+import java.util.Scanner;
+
+public class Codeup1071 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while(true) {
+            int a = sc.nextInt();
+            if (a == 0)
+                break;
+            else System.out.println(a);
+        }
+
+    }
+}
