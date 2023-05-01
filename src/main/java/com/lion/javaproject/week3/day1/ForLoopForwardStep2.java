6ec3a27f47373c0813a50ad462cee2146b32c349
xxo <xxoxec@gmail.com>
2023-05-01 13:15:11 +0900

    feat: ForLoopForwardStep - i값을 2씩 늘리기 홀수, 짝수

---
+++ src/main/java/com/lion/javaproject/week3/day1/ForLoopForwardStep2.java
@@ -0,0 +1,10 @@
+package com.lion.javaproject.week3.day1;
+
+public class ForLoopForwardStep2 {
+    public static void main(String[] args) {
+        for (int i = 1; i < 10; i += 2) {
+            System.out.println(i);
+
+        }
+    }
+}
