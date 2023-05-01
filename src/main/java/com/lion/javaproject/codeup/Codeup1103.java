23f8d2c786dc40240792130241ed7e21ff4bfa8e
xxo <xxoxec@gmail.com>
2023-05-01 09:14:13 +0900

    feat: codeup - 1103, 1106

---
+++ src/main/java/com/lion/javaproject/codeup/Codeup1103.java
@@ -0,0 +1,11 @@
+package com.lion.javaproject.codeup;
+
+import java.util.Scanner;
+
+public class Codeup1103 {
+    public static void main(String[] args) {
+        System.out.println("\"c:\\test\"");
+
+
+    }
+}
---
+++ src/main/java/com/lion/javaproject/codeup/Codeup1106.java
@@ -0,0 +1,12 @@
+package com.lion.javaproject.codeup;
+
+public class Codeup1106 {
+    public static void main(String[] args) {
+
+        int min,max;
+        min = Integer.MIN_VALUE;
+        max = Integer.MAX_VALUE;
+        System.out.print(min + " " + max);
+
+    }
+}
