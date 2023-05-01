855ce32fda02c468802fc85334c566c9284a9bd5
xxo <xxoxec@gmail.com>
2023-05-01 11:26:16 +0900

    feat: ForLoopReverse - 5에서 1까지 출력 reverse

--- src/main/java/com/lion/javaproject/week3/day1/ForLoopReverse.java
+++ src/main/java/com/lion/javaproject/week3/day1/ForLoopReverse.java
@@ -3,7 +3,8 @@ package com.lion.javaproject.week3.day1;
 public class ForLoopReverse {
     public static void main(String[] args) {
         for (int i = 5; i > 0; i--) {
-            System.out.println(i);
+            //System.out.println(i);
+            System.out.printf("%d ", i); // 가로
         }
     }
 }
