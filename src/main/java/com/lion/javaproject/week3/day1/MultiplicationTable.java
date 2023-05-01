e95399bce9f4aa6483b6e09ffbfb72d8db64af6b
xxo <xxoxec@gmail.com>
2023-05-01 14:37:01 +0900

    feat: MultiplicationTable - 구구단2-4

--- src/main/java/com/lion/javaproject/week3/day1/MultiplicationTable.java
+++ src/main/java/com/lion/javaproject/week3/day1/MultiplicationTable.java
@@ -2,8 +2,11 @@ package com.lion.javaproject.week3.day1;
 
 public class MultiplicationTable {
     public static void main(String[] args) {
-        for (int i = 1; i <= 9; i++) {
-            System.out.printf("2 * %d = %d\n", i, 2 * i);
+        for (int j = 2; j <= 4; j++) {
+            for (int i = 1; i <= 9; i++) {
+                System.out.printf("%d * %d = %d\n",j, i, i * j);
+            }
+            System.out.println();
         }
     }
 }
