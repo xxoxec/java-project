ccce52372ce6f53f240dba0ae3a4f39fa3bd788c
xxo <xxoxec@gmail.com>
2023-05-01 09:38:09 +0900

    feat: NewSwitchCaseDaysOfMonth - switch 표현식의 결과를 변수로

---
+++ src/main/java/com/lion/javaproject/week3/day1/NewSwitchCaseDaysOfMonth.java
@@ -0,0 +1,16 @@
+package com.lion.javaproject.week3.day1;
+
+public class NewSwitchCaseDaysOfMonth {
+    public static void main(String[] args) {
+        int month = 11;
+        int lastDate = switch(month) {
+            case 1, 3, 5, 7, 8, 10, 12 -> 31;
+            case 4, 6, 9, 11 -> 30;
+            case 2 -> 28;
+            default -> throw new IllegalArgumentException("잘못된 월:" +
+                    month);
+        };
+        System.out.printf("%d월은 %d일 까지 있습니다.\n", month,lastDate);
+        System.out.println(lastDate);
+    }
+}
