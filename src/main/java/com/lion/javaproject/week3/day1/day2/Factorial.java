71a00c4a8085b6b39f503123a174bb0039ddc8f4
xxo <xxoxec@gmail.com>
2023-05-02 09:50:03 +0900

    feat: n! - 5-1, 1-5

---
+++ src/main/java/com/lion/javaproject/week3/day1/day2/Factorial.java
@@ -0,0 +1,16 @@
+package com.lion.javaproject.week3.day1.day2;
+
+public class Factorial {
+    public static void main(String[] args) {
+        int n = 5;
+        int answer = 1;
+        for( int i = n; i >= 1; i--) {
+            System.out.println(i);
+
+//        int n = 5;
+//        int answer = 1;
+//        for(int i = 1; i <= n; i++){
+//            System.out.println(i);
+        }
+    }
+}
