468f472268472a067610d3622be63d400d0f1417
xxo <xxoxec@gmail.com>
2023-05-04 09:29:49 +0900

    feat: 소인수분해

---
+++ src/main/java/com/lion/javaproject/week3/day1/day4/P120852B.java
@@ -0,0 +1,25 @@
+package com.lion.javaproject.week3.day1.day4;
+
+public class P120852B {
+    public static void main(String[] args) {
+        int n =12;
+        int divisor = 2;
+        int[] arr = new int[n + 1];
+
+//        for (int i = 2; n > 0; i++) {
+//            if (n % i == 0) {
+//                arr[divisor] = i;
+//                n /= i;
+
+        int idx = 0;
+        while(n > 1) {
+            if(n % divisor == 0) {
+                arr[idx++] = divisor;
+                n /= divisor;
+            } else {
+                divisor++;
+            }
+        }
+        for  (int i = 0; i < arr.length; i++) System.out.printf("%d " ,arr[i]);
+    }
+}
